44e223ed5ac3df9d347bb7a9ca6caef5db64b842
braiazx <nikolasbrian08@gmail.com>
2025-07-25 11:46:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List, Dict
+import uvicorn
+import os
 
 app = FastAPI()
 
@@ -15,3 +17,7 @@ async def limpar_planilha(data: PlanilhaEntrada):
         if nova_linha:
             dados_limpos.append(nova_linha)
     return {"rows_limpos": dados_limpos}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
